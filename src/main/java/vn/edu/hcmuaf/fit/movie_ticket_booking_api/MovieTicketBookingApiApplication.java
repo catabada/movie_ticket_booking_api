d6b1c142db90935d017878298efd2601ed294a36
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-04-25 00:20:18 +0700

    fix gradle file

--- src/main/java/vn/edu/hcmuaf/fit/movie_ticket_booking_api/MovieTicketBookingApiApplication.java
+++ src/main/java/vn/edu/hcmuaf/fit/movie_ticket_booking_api/MovieTicketBookingApiApplication.java
@@ -1,7 +1,6 @@
 package vn.edu.hcmuaf.fit.movie_ticket_booking_api;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import vn.edu.hcmuaf.fit.movie_ticket_booking_api.repository.app_role.AppRoleRepository;
 
 import java.util.Arrays;
 
@@ -17,17 +15,9 @@ import java.util.Arrays;
 @Slf4j
 @EnableAsync
 public class MovieTicketBookingApiApplication {
-	private static AppRoleRepository appRoleRepository;
-
-	@Autowired
-	public void setAppRoleRepository(AppRoleRepository appRoleRepository) {
-		MovieTicketBookingApiApplication.appRoleRepository = appRoleRepository;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieTicketBookingApiApplication.class, args);
-//		appRoleRepository.save(AppRole.builder().name(RoleConstant.ROLE_MEMBER).build());
-//		appRoleRepository.save(AppRole.builder().name(RoleConstant.ROLE_ADMIN).build());
 	}
 
 	@Bean
