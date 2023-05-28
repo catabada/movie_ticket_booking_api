f28ce285ca2318272615f340ddd0fe43caf640d6
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-05-29 02:41:22 +0700

    database

--- src/main/resources/database/room.sql
+++ src/main/resources/database/room.sql
@@ -1,3 +1,6 @@
 -- ROOM
 insert into room (name, row, col, total_seat, room_state, room_type, branch_id, state)
-values ('Rạp 1', 10, 25, 250, 'AVAILABLE', 'VIP', 26, 'ACTIVE');
+values ('Rạp 1', 10, 25, 250, 'AVAILABLE', 'VIP', 26, 'ACTIVE'),
+       ('Rạp 2', 10, 25, 250, 'AVAILABLE', 'VIP', 26, 'ACTIVE'),
+       ('Rạp 3', 10, 25, 250, 'AVAILABLE', 'VIP', 26, 'ACTIVE'),
+       ('Rạp 5', 10, 25, 250, 'AVAILABLE', 'VIP', 26, 'ACTIVE');
